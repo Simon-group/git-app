6779baead2c56eb2f3002d210837fa0ed350f021
simon-group <saku_crownheart@yahoo.co.jp>
2019-11-02 18:46:29 +0900

    test

--- app/test.java
+++ app/test.java
@@ -1,5 +1,8 @@
 public class test{
   public static void main(String[] args){
-    System.out.println("おはよう");
+    int month = 12;
+    int date = 31;
+
+    System.out.println(month + "月" + date + "日");
   }
 }
