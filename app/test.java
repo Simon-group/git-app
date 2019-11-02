a62b2675326c215c0594ee2bfd55b6691b09689f
simon-group <saku_crownheart@yahoo.co.jp>
2019-11-03 02:24:41 +0900

    progate

--- app/test.java
+++ app/test.java
@@ -1,21 +1,12 @@
 public class test{
   public static void main(String[] args){
     // 変数nameに、好きな名前を代入してください
-    int number = 12;
+    int number = 10;
     
-    // switch文を用いて、numberを3で割った余りで条件分岐をしてください
-    switch (number % 5) {
-      case 0:
-        System.out.println("大吉です");
-        break;
-      case 1:
-        System.out.println("中吉です");
-        break;
-      case 4:
-        System.out.println("凶です");
-        break;
-      default:
-        System.out.println("吉です");
+    // while文を用いて、numberが0より大きい場合に繰り返す、繰り返し処理を作ってください
+    while (number > 0){
+      System.out.println(number);
+      number--;
     }
   }
 }
