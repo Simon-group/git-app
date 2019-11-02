ceebfdb26fad2eccd0d02b6dbe3ff87dced120d9
simon-group <saku_crownheart@yahoo.co.jp>
2019-11-03 02:20:20 +0900

    progate

--- app/test.java
+++ app/test.java
@@ -4,16 +4,18 @@ public class test{
     int number = 12;
     
     // switch文を用いて、numberを3で割った余りで条件分岐をしてください
-    switch(number % 3){
+    switch (number % 5) {
       case 0:
-      System.out.println("3で割り切れます");
-      break;
+        System.out.println("大吉です");
+        break;
       case 1:
-      System.out.println("3で割ると1余ります");
-      break;
-      case 2:
-      System.out.println("3で割ると2余ります");
-      break;
+        System.out.println("中吉です");
+        break;
+      case 4:
+        System.out.println("凶です");
+        break;
+      default:
+        System.out.println("吉です");
     }
   }
 }
