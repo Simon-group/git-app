d3d667eb9154927afacbae1845f81ad51cd63005
simon-group <saku_crownheart@yahoo.co.jp>
2019-11-02 19:06:27 +0900

    progate8

--- app/test.java
+++ app/test.java
@@ -1,10 +1,25 @@
 public class test{
   public static void main(String[] args){
-    int number1 = 7;
-    int number2 = 2;
-
-    System.out.println(number1 / number2);
-
-    System.out.println((double)number1 / number2);
+    // 変数nameに、好きな名前を代入してください
+    String name = "Simon";
+    
+    // 変数ageに、好きな年齢を代入してください
+    int age = 29;
+    
+    // 変数heightに、好きな身長を代入してください（例: 1.2）
+    double height = 1.69;
+    
+    // 変数weightに、好きな体重を代入してください（例: 20.0）
+    double weight = 60.4;
+    
+    // 変数bmiに、BMIを計算して代入してください
+    double bmi = weight / height / height;
+    
+    // 名前、年齢、身長、体重、BMIに関する情報を出力してください
+    System.out.println("名前は" + name + "です");
+    System.out.println("年齢は" + age + "歳です");
+    System.out.println("身長は" + height + "mです");
+    System.out.println("体重は" + weight + "kgです");
+    System.out.println("BMIは" + bmi + "です");
   }
 }
