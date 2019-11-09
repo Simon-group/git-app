aaf34502a99b6221e3dae5a9c79dbba8d1a93c11
simon-group <saku_crownheart@yahoo.co.jp>
2019-11-09 15:21:50 +0900

    study

--- app/Test.java
+++ app/Test.java
@@ -3,23 +3,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class Test {
+class Test {
     public static void main(String[] args) {
-        System.out.println("数当てゲーム");
-        int ans = new java.util.Random().nextInt(10);
-        System.out.println("ヒント: "+ ans);
-        for(int i = 0;i < 5;i++){
-          System.out.println("0〜9の数字を入力してください");
-          int num = new java.util.Scanner(System.in).nextInt();
-          if(num == ans && num <= 9){
-            System.out.println("アタリ!");
-          }else if(num != ans && num <= 9){
-            System.out.println("違います。");
-          }
-          else {
-            System.out.println("指定の数値(0~9)を入力して下さい");
-          }
-        }
-        System.out.println("ゲームを終了します");
-      }
+      int[] score =  {1, 1, 1, 1};
+      System.out.println(score[0]);
+      System.out.println(score[1]);
+      System.out.println(score[2]);
+      System.out.println(score[3]);
     }
+  }
