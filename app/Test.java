139c2a842b49b757b9ace9f74f9ee9f2dc055d40
simon-group <saku_crownheart@yahoo.co.jp>
2019-11-09 14:36:13 +0900

    progate

--- app/Test.java
+++ app/Test.java
@@ -5,21 +5,21 @@ import java.io.PrintWriter;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("[メニュー]1:検索2:登録3:削除4:変更");
-        int selected = new java.util.Scanner(System.in).nextInt();
-        switch(selected){
-          case 1:
-            System.out.println("検索します");
-            break;
-          case 2:
-            System.out.println("登録します");
-            break;
-          case 3:
-            System.out.println("削除します");
-            break;
-          case 4:
-            System.out.println("変更します");
-            break;
+        System.out.println("数当てゲーム");
+        int ans = new java.util.Random().nextInt(10);
+        System.out.println("ヒント: "+ ans);
+        for(int i = 0;i < 5;i++){
+          System.out.println("0〜9の数字を入力してください");
+          int num = new java.util.Scanner(System.in).nextInt();
+          if(num == ans && num <= 9){
+            System.out.println("アタリ!");
+          }else if(num != ans && num <= 9){
+            System.out.println("違います。");
+          }
+          else {
+            System.out.println("指定の数値(0~9)を入力して下さい");
+          }
         }
+        System.out.println("ゲームを終了します");
+      }
     }
-}
