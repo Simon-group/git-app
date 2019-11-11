a5c216ae7434b79616e23cdd4b976042c148f052
simon-group <saku_crownheart@yahoo.co.jp>
2019-11-11 21:54:37 +0900

    progate

--- app/Test.java
+++ app/Test.java
@@ -1,15 +1,10 @@
 
 class Test {
   public static void main(String[] args) {
-    // fullNameメソッドの結果を変数nameに代入してください
-    String name = fullName("Kate","Jones");
+    printData(fullName("Kate", "Jones"), 27);
     
-    // printDataの引数を書き換えてください
-    printData(name, 27);
-    
-    // こちらは書き換えないでください
-    printData("John Christopher Smith", 65);
-    
+    // fullNameメソッドを用いて、printDataの引数を書き換えてください
+    printData(fullName("John","Christopher","Smith"),100);
   }
 
   public static void printData(String name, int age) {
@@ -17,9 +12,12 @@ class Test {
     System.out.println("年齢は" + age + "歳です");
   }
 
-  // fullNameメソッドを定義してください
-  public static String fullName(String firstName,String lastName){
+  public static String fullName(String firstName, String lastName) {
     return firstName + " " + lastName;
   }
   
+  // fullNameメソッドを定義してください
+  public static String fullName(String firstName, String middleName, String lastName) {
+    return firstName + " " + middleName + " " + lastName;
+  }
 }
