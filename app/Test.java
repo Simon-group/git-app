822f431b409b98a3e709201d41a43392ef411f1e
simon-group <saku_crownheart@yahoo.co.jp>
2019-11-12 21:35:30 +0900

    progate

--- app/Test.java
+++ app/Test.java
@@ -1,5 +1,7 @@
 public class Test {
   public static void main(String[] args) {
-    Test2.hello();
+    // printDataメソッドとfullNameメソッドの呼び出しを書き換えてください
+    Test2.printData(Test2.fullName("Kate", "Jones"), 27, 1.6, 50.0);
+    Test2.printData(Test2.fullName("John", "Christopher", "Smith"), 65, 1.75, 80.0);
   }
 }
