f9358fcbe77054e27612f94392e9ff9ff099f236
simon-group <saku_crownheart@yahoo.co.jp>
2019-11-13 21:46:34 +0900

    javarurning

--- app/Test.java
+++ app/Test.java
@@ -1,20 +1,23 @@
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Test {
   public static void main(String[] args) {
-    //ListからMapへ変換
-    //Listの宣言
-    List<String> list = new ArrayList<>(Arrays.asList("chocolate", "makaron", "cheesecake", "pudding", "cookie", "pancake"));
+    Map map = new HashMap(); 
+    map.put("Samurai", "Engineer");
+    map.put("Soldier", "Warrior");
     
-    System.out.println(list.toString());
-    
-    //Mapの宣言
-    Map<String, Integer> map = list.stream().collect(Collectors.toMap(s -> s, s -> s.length()));
-
     System.out.println(map.toString());
+    
+    // 1. values()でマップの全値を取得する
+    List listValues = new ArrayList(map.values());
+    
+    // 2. keySet()でマップのキー値をすべて取得する
+    List listKeys = new ArrayList(map.keySet());
+
+    System.out.println(listValues.toString());
+    System.out.println(listKeys.toString());
   }
 }
