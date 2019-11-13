fb3a5348a2abb9495d260a5ff3e4447f413501ec
simon-group <saku_crownheart@yahoo.co.jp>
2019-11-13 21:35:36 +0900

    javamaplist

--- app/Test.java
+++ app/Test.java
@@ -1,25 +1,20 @@
+import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Test {
   public static void main(String[] args) {
+    //ListからMapへ変換
     //Listの宣言
-    List<Integer> list = new ArrayList<>();
+    List<String> list = new ArrayList<>(Arrays.asList("chocolate", "makaron", "cheesecake", "pudding", "cookie", "pancake"));
     
-    list.add(1);
-    list.add(2);
-    list.add(3);
+    System.out.println(list.toString());
     
     //Mapの宣言
-    Map<Integer, String> map = new HashMap<>();
-    
-    for(Integer num : list) {
-        // MapにListのキーと値を追加
-        map.put(num, "apple");
-    }
-    
-    System.out.println(map); 
+    Map<String, Integer> map = list.stream().collect(Collectors.toMap(s -> s, s -> s.length()));
+
+    System.out.println(map.toString());
   }
 }
